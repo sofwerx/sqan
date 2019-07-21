78021f434eeea9d4f62431a7eecb2d27b21dc620
jvinall <jvinall@augmentedengagement.com>
2019-07-20 22:29:16 -0700

    - SqANDR config testing app WIP

--- testing/sqandr/app/src/main/java/org/sofwerx/sqandr/testing/MainActivity.java
+++ testing/sqandr/app/src/main/java/org/sofwerx/sqandr/testing/MainActivity.java
@@ -2,9 +2,14 @@ package org.sofwerx.sqandr.testing;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.sofwerx.sqan.listeners.PeripheralStatusListener;
 import org.sofwerx.sqandr.SqANDRListener;
@@ -13,47 +18,109 @@ import org.sofwerx.sqandr.serial.TestService;
 public class MainActivity extends AppCompatActivity implements SqANDRListener, PeripheralStatusListener {
     private TestService testService;
     private TextView textStatus;
-    private ImageView imageStatus;
-    private PlutoStatus status = PlutoStatus.INSTALLING;
+    private EditText editCommands;
+    private ImageView imagePlutoStatus,imageAppStatus;
+    private PlutoStatus plutoStatus = PlutoStatus.OFF;
+    private AppStatus appStatus = AppStatus.OFF;
 
-    private enum PlutoStatus {ERROR,INSTALLING,UP}
+    private enum PlutoStatus {OFF,ERROR,INSTALLING,UP}
+    private enum AppStatus {OFF,HIDE,RUNNING}
 
-    private void setStatus(String message) {
-        runOnUiThread(() -> textStatus.setText(message));
-    }
-
-    private void setStatus(PlutoStatus status) {
-        if (this.status != status) {
-            this.status = status;
-            runOnUiThread(() -> {
-                switch (status) {
-                    case UP:
-                        imageStatus.setImageResource(R.drawable.icon_up);
-                        break;
-
-                    case INSTALLING:
-                        imageStatus.setImageResource(R.drawable.icon_busy);
-                        break;
-
-                    default:
-                        imageStatus.setImageResource(R.drawable.icon_error);
-                }
-            });
-        }
-    }
+    private final static String PREFS_COMMANDS = "cmds";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textStatus = findViewById(R.id.mainStatus);
-        imageStatus = findViewById(R.id.mainStatusIcon);
+        imagePlutoStatus = findViewById(R.id.mainStatusIcon);
+        imageAppStatus = findViewById(R.id.mainStatusStartButton);
+        imageAppStatus.setOnClickListener(v -> toggleAppStatus());
+        editCommands = findViewById(R.id.mainCommands);
         testService = new TestService(this);
+    }
+
+    private void loadPrefs() {
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        editCommands.setText(sharedPrefs.getString(PREFS_COMMANDS,null));
+    }
+
+    private void savePrefs() {
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        SharedPreferences.Editor edit = sharedPrefs.edit();
+        edit.putString(PREFS_COMMANDS,editCommands.getText().toString());
+        edit.apply();
+    }
+
+    private void setPlutoStatus(String message) {
+        runOnUiThread(() -> textStatus.setText(message));
+    }
+
+    private void toggleAppStatus() {
+        //TODO
+    }
+
+    private void setStatus(AppStatus status) {
+        if (this.appStatus != status) {
+            this.appStatus = status;
+            runOnUiThread(() -> {
+                switch (appStatus) {
+                    case OFF:
+                        imageAppStatus.setImageResource(R.drawable.icon_up);
+                        imageAppStatus.setVisibility(View.VISIBLE);
+                        editCommands.setEnabled(true);
+                        Toast.makeText(MainActivity.this,"SqANDR is off",Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case RUNNING:
+                        imageAppStatus.setImageResource(R.drawable.icon_busy);
+                        imageAppStatus.setVisibility(View.VISIBLE);
+                        editCommands.setEnabled(false);
+                        Toast.makeText(MainActivity.this,"SqANDR is running",Toast.LENGTH_SHORT).show();
+                        break;
+
+                    default:
+                        imageAppStatus.setVisibility(View.INVISIBLE);
+                        editCommands.setEnabled(true);
+                }
+            });
+        }
+    }
+
+    private void setStatus(PlutoStatus status) {
+        if (this.plutoStatus != status) {
+            this.plutoStatus = status;
+            runOnUiThread(() -> {
+                switch (status) {
+                    case UP:
+                        imagePlutoStatus.setImageResource(R.drawable.icon_up);
+                        Toast.makeText(MainActivity.this,"Pluto is ready",Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case INSTALLING:
+                        imagePlutoStatus.setImageResource(R.drawable.icon_busy);
+                        setStatus(AppStatus.HIDE);
+                        Toast.makeText(MainActivity.this,"SqANDR is installing",Toast.LENGTH_LONG).show();
+                        break;
+
+                    case OFF:
+                        imagePlutoStatus.setImageResource(R.drawable.icon_off);
+                        Toast.makeText(MainActivity.this,"Pluto is not connected",Toast.LENGTH_LONG).show();
+                        setStatus(AppStatus.HIDE);
+                        break;
+
+                    default:
+                        imagePlutoStatus.setImageResource(R.drawable.icon_error);
+                        setStatus(AppStatus.HIDE);
+                }
+            });
+        }
     }
 
     @Override
     public void onResume() {
         super.onResume();
+        loadPrefs();
         if (testService != null) {
             testService.setListener(this);
             testService.setPeripheralStatusListener(this);
@@ -66,6 +133,7 @@ public class MainActivity extends AppCompatActivity implements SqANDRListener, P
             testService.setListener(null);
             testService.setPeripheralStatusListener(this);
         }
+        savePrefs();
         super.onPause();
     }
 
@@ -80,27 +148,29 @@ public class MainActivity extends AppCompatActivity implements SqANDRListener, P
 
     @Override
     public void onSdrError(String message) {
-        setStatus("ERROR: "+message);
+        setPlutoStatus("ERROR: "+message);
         setStatus(PlutoStatus.ERROR);
         //TODO
     }
 
     @Override
     public void onSdrReady(boolean isReady) {
-        setStatus("SDR is ready");
+        setPlutoStatus("Pluto is ready");
+        setStatus(AppStatus.RUNNING);
         //TODO
     }
 
     @Override
     public void onSdrMessage(String message) {
-        setStatus(message);
+        setPlutoStatus(message);
+        setStatus(PlutoStatus.INSTALLING);
         //TODO
     }
 
     @Override
     public void onPacketReceived(byte[] data) {
         //TODO
-        setStatus(PlutoStatus.UP);
+        setStatus(AppStatus.RUNNING);
     }
 
     @Override
@@ -110,18 +180,18 @@ public class MainActivity extends AppCompatActivity implements SqANDRListener, P
 
     @Override
     public void onPeripheralMessage(String message) {
-        setStatus(message);
+        setPlutoStatus(message);
     }
 
     @Override
     public void onPeripheralReady() {
-        setStatus("Pluto is ready");
+        setPlutoStatus("Pluto is ready");
         setStatus(PlutoStatus.UP);
     }
 
     @Override
     public void onPeripheralError(String message) {
-        setStatus("ERROR: "+message);
+        setPlutoStatus("ERROR: "+message);
         setStatus(PlutoStatus.ERROR);
     }
 }
